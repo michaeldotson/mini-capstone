f5da7c84bf6b4d1f4da985a179a32a8a4112ab90
mike <michaeldotson@protonmail.com>
2019-01-25 09:50:38 -0800

    corrected ActiveREcord methods

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,21 +1,42 @@
 class Api::ProductsController < ApplicationController
 
   def index
-    @products = Product.all
-    render 'index.json.jbuilder'
-  end
+      @products = Product.all.order(:id)
+      search_term = params[:search]
+      if search_term
+        @products = Product.where("name LIKE ?", "%#{search_term}%")
+      end
+      discount = params[:discount]
+      if discount 
+        @products = Product.where("price < ?", 50)
+      end
+
+
+      sort_term = params[:sort]
+      sort_order = params[:sort_order]
+      if sort_term == "price"
+        if sort_order == "desc"
+          @products = @products.order(price: :desc)
+        else
+          @products = @products.order(:price)
+        end
+      else
+        @products = @products.order(:id)
+      end
+      render 'index.json.jbuilder'
+    end
 
   def show
-    @product = Product.find_by(id: params["id"])
+    @product = Product.find(params[:id])
     render 'show.json.jbuilder'
   end
 
   def create
     @product = Product.new(
-    name: params["name"],
-    price: params["price"],
-    description: params["description"],
-    image_url: params["image_url"]
+    name: params[:name],
+    price: params[:price],
+    description: params[:description],
+    image_url: params[:image_url]
     
     )
     
@@ -30,10 +51,10 @@ class Api::ProductsController < ApplicationController
 
   def update
     @product = Product.find(params[:id])
-    @product.name = params["name"] || @product.name
-    @product.price = params["price"] || @product.price
-    @product.image_url = params["image_url"] || @product.image_url
-    @product.description = params["description"] || @product.description
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.image_url = params[:image_url] || @product.image_url
+    @product.description = params[:description] || @product.description
     
     if @product.save
       #happy path
